efe1fc5f919ff444435c99600ba622e913358380
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-03-17 21:51:01 -0500

    updating connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "	nebpbnkk61jardf0",
+    user: "nebpbnkk61jardf0",
     password: "d12n0o5kbtdrn019",
     database: "y7znuhivpj3ldlf0"
 });
